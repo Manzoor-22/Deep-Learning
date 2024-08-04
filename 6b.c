4266465296da8bc81c045cbdede3bfc43fff3c54
Manzoor <manzoormohd.022@gmail.com>
2024-08-05 00:29:45 +0530

    5-8

--- 6b.c
+++ 6b.c
@@ -1,12 +1,11 @@
-#include"netinet/in.h"
-#include"sys/socket.h"
-#include"stdio.h"
-#include"string.h"
-#include"time.h"
-#include"stdlib.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main( )
-{
+int main(){
     struct sockaddr_in sa;
     struct sockaddr_in cli;
     int sockfd,conntfd;
@@ -17,31 +16,34 @@ int main( )
     sockfd=socket(AF_INET,SOCK_STREAM,0);
     
     if(sockfd<0){
-        printf("error in socket\n");
+        printf("\nError in socket");
         exit(0);
     }
-    else 
-        printf("Socket opened");
-        
+    else{
+        printf("\nSocket opened");
+    }    
+    
     bzero(&sa,sizeof(sa));
     sa.sin_port=htons(5600);
     sa.sin_addr.s_addr=htonl(0);
     
     if(bind(sockfd,(struct sockaddr*)&sa,sizeof(sa))<0){
-        printf("Error in binding\n");
+        printf("\nError in binding");
+        exit(0);
     }
-    else
-        printf("Binded Successfully");
-        
+    else{
+        printf("\nBinded Successfully");
+    }
+    
     listen(sockfd,50);
     
-    for(;;)
-    {
+    for(;;){
         len=sizeof(ch);
         conntfd=accept(sockfd,(struct sockaddr*)&cli,&len);
         printf("Accepted");
         tick=time(NULL);
         snprintf(str,sizeof(str),"%s",ctime(&tick));
-        printf("%s",str);write(conntfd,str,100);
+        printf("%s",str);
+        write(conntfd,str,100);
     }
 }
